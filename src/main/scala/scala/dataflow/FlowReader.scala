54b48deb4a3bdc48fecb88a8c6e7c9719d2f432a
Heather Miller <heather.miller@epfl.ch>
2012-04-04 15:05:54 +0200

    Added some comments

--- src/main/scala/scala/dataflow/FlowReader.scala
+++ src/main/scala/scala/dataflow/FlowReader.scala
@@ -4,15 +4,27 @@ package scala.dataflow
 
 
 
-
+/**
+ * Basically an iterator. Possible to get it out of different flow collections. Basically describe a sequence of incoming elements.
+ */
 trait FlowReaderLike[T, Async[X]] {
   
+/**
+ * Look at the next element, but don't take it from the next `FlowReader`
+ * Useful if you have 2 incoming sorted buffer and you want to merge them to create a single sorted buffer. `peek` would allow you to check the head of both and determine which to actually `pop` (take)
+ */
   def peek: Async[T]
   
+  /**
+  * Analogous to the `next` method on iterators.
+  */
   def pop(): Async[T]
   
   def isEmpty: Boolean
   
+  /**
+  *  
+  */
   def blocking: FlowReader.Blocking[T]
   
 }
