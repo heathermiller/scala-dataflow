a6d6ed2ce46557c1d3be15ecf239e68975e4415c
Aleksandar Prokopec <axel22@gmail.com>
2012-04-04 15:50:28 +0200

    FlowVar in the impl package.

--- src/main/scala/scala/dataflow/FlowMap.scala
+++ src/main/scala/scala/dataflow/FlowMap.scala
@@ -9,12 +9,32 @@ trait FlowMapLike[K, V, Async[X]] extends FlowLike[(K, V)] {
   
   def apply(key: K): Async[V]
   
+  /** Gets the value associated with the key.
+   *  The value is available only once the key is added to the flow map,
+   *  or the flow map is sealed, in which case `get` returns a `None`.
+   *  
+   *  {{{
+   *  val v1: Future[Option[Int]] = fm.get(1)
+   *  val v2: Option[Int] = fm.blocking.get(1)
+   *  }}}
+   */
   def get(key: K): Async[Option[V]]
   
   def update(key: K, value: V): Unit
   
   def seal(): FlowMap.Blocking[K, V]
   
+  /** A foreach loop on all the elements.
+   *  Completes when the map is sealed.
+   *  
+   *  {{{
+   *  (for (kv <- fm) {
+   *    process(kv)
+   *  }) andThen {
+   *    finish()
+   *  }
+   *  }}}
+   */
   def foreach[U](f: (K, V) => U): Async[Unit]
   
   def onKey[U](key: K)(body: V => U): Unit
