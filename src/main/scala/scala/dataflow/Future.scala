e4ba775b51df8e52c504db354a4df93b0d2f85db
Tobias Schlatter <tobias@meisch.ch>
2012-05-13 19:55:25 +0200

    Added private modifiers to Future's vars

--- src/main/scala/scala/dataflow/Future.scala
+++ src/main/scala/scala/dataflow/Future.scala
@@ -5,8 +5,8 @@ import scala.collection.mutable.MutableList
 // TODO do we need CAS here or are synchronized methods enough? -- tobias
 class Future[T] {
 
-  var res: Option[T] = None
-  var cbs: MutableList[T => Unit] = new MutableList()
+  private var res: Option[T] = None
+  private var cbs: MutableList[T => Unit] = new MutableList()
 
   def map[U](f: T => U): Future[U] = {
     val fut = new Future[U]
