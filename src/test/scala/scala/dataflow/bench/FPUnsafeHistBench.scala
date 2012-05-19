bd3c4afd2420913ae9cf75614620b057f02917f6
Tobias Schlatter <tobias@meisch.ch>
2012-05-19 16:02:35 +0200

    Added Unsafe (but fast) FPHistBench

--- src/test/scala/scala/dataflow/bench/FPUnsafeHistBench.scala
+++ src/test/scala/scala/dataflow/bench/FPUnsafeHistBench.scala
@@ -3,7 +3,7 @@ package scala.dataflow.bench
 import scala.dataflow._
 import scala.util.Random
 
-object FPHistBench extends testing.Benchmark with Utils.Props {
+object FPUnsafeHistBench extends testing.Benchmark with Utils.Props {
   import Utils._
 
   val maxval = 100
@@ -30,14 +30,12 @@ object FPHistBench extends testing.Benchmark with Utils.Props {
   }
 
   private def binning(count: Int, pool: FlowPool[Data]) = {
-    val init = Map[Int,Int]()
-    val fm = pool.mappedFold(init)(mergeMaps _)(x => Map(x.i * count / maxval -> 1))
-    fm.map {
-      case (cnt,m) => for (i <- 0 to (count - 1)) yield m.getOrElse(i,0)
-    }
+    // this is fast but unsafe
+    val agg = Array.fill[Int](count)(0)
+    pool.doForAll { x =>
+      val ind = x.i * count / maxval
+      agg(ind) = agg(ind) + 1            
+    } map { x => agg }
   }
-
-  private def mergeMaps(m1: Map[Int,Int], m2: Map[Int,Int]) = 
-    m1 ++ m2.map { case (k,v) => k -> (v + m1.getOrElse(k,0)) }
   
 }
