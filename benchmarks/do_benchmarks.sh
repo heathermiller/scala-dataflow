636d52354d1d040186d1731daf76316dc1ed25f5
Tobias Schlatter <tobias@meisch.ch>
2012-05-19 16:09:03 +0200

    Preparing for new benchmarks

--- benchmarks/do_benchmarks.sh
+++ benchmarks/do_benchmarks.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-CURV="v1"
+CURV="v2"
 NOW=$(date '+%Y-%m-%dT%H.%M.%S')
-PARS=$(seq 1 8)
-LENS=$(seq 100000 50000 1000000)
-BENCHES="ConcurrentLinkedQueueBench FlowPoolBench FlowPoolExperiment"
-N=100
+PARS=$(seq 1 1)
+LENS=$(seq 100000 10000 500000)
+BENCHES="FPInsertBench CLQInsertBench LTQInsertBench FPReduceBench FPSealedInsertBench FPUnsafeHistBench FPHistBenchg LTQHistBench LTQReduceBench"
+N=20
 
 java -version > "data/${NOW}_javav.txt" 2>&1
 
@@ -14,8 +14,8 @@ cd ..
 for par in $PARS; do
     for l in $LENS; do
         for bench in $BENCHES; do
-            bdat=$(sbt "bench -Dsize=$l -Dpar=$par scala.dataflow.$bench $N" | \
-                grep '^scala.dataflow')
+            bdat=$(sbt "bench -Dsize=$l -Dpar=$par scala.dataflow.bench.$bench $N" | \
+                grep '^scala.dataflow.bench')
             echo "$CURV\t$bench\t$par\t$l\t$bdat"
         done
     done
