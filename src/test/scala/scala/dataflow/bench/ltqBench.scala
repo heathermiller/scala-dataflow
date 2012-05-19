7790240fe80fd451dda569523fa9f6c948573d76
Tobias Schlatter <tobias@meisch.ch>
2012-05-19 14:09:29 +0200

    Added histogram benchmark

--- src/test/scala/scala/dataflow/bench/ltqBench.scala
+++ src/test/scala/scala/dataflow/bench/ltqBench.scala
@@ -2,3 +2,4 @@ package scala.dataflow.bench
 
 object LTQInsertBench extends QInsertBench with LTQBuilder
 object LTQReduceBench extends QReduceBench with LTQBuilder
+object LTQHistBench extends QHistBench with LTQBuilder
