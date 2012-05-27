c9941b51a2f0da3aff248021565194cc512697f3
Tobias Schlatter <tobias@meisch.ch>
2012-05-27 22:08:01 +0200

    Add more sophisticated checks to MLFPTest

--- src/test/scala/scala/dataflow/MLFPTest.scala
+++ src/test/scala/scala/dataflow/MLFPTest.scala
@@ -2,18 +2,23 @@ package scala.dataflow
 
 import java.lang.Thread
 import java.util.concurrent.ThreadLocalRandom
+import scala.collection.mutable.{ Set => MSet }
 
 object MLFPTest extends App {
 
-  val tasks = 100
+  val tasks = 50
   val n = 1000
   val pool = new MultiLaneFlowPool[(Int,Int)](tasks/2)
   val b = pool.builder
 
-  val r = pool.mappedFold(0)(_ + _)(x => x._2)
-  val s = pool.mappedFold(0)(_ + _)(x => x._1)
+  val vals = MSet.empty[(Int,Int)]
 
-  b.seal(n * tasks)
+  val sfill = pool.doForAll(vals += _)
+  val rf = pool.mappedFold(0)(_ + _)(x => x._2)
+  val sf = pool.mappedFold(0)(_ + _)(x => x._1)
+
+  val rc = n * (n + 1) / 2 * tasks
+  val sc = tasks * (tasks + 1) * n
 
   for (i <- 1 to tasks) {
     task {
@@ -25,7 +30,19 @@ object MLFPTest extends App {
     }
   }
 
-  println("Done: %d, %d".format(s.blocking._2, r.blocking._2))
+  b.seal(n * tasks)
+
+  val rv = rf.blocking._2
+  val sv = sf.blocking._2
+  sfill.blocking
+
+  val vals_should = ( for (i <- 1 to tasks ; j <- 1 to n) yield (i,j) ) toSet
+
+  val missing = vals_should.diff(vals).size
+
+  println("%d values missing (diff)".format(missing))
+  println("%d values missing (sub)".format(vals_should.size - vals.size))
+  println("Done: r: %d (should: %d), s: %d (should: %d)".format(rv,rc,sv,sc))
 
   def task(f: => Unit) {
     new Thread(new Runnable() { def run() = f }).start()
