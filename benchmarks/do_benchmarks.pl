87e7ac358292dadfc2802053aca793393f74a408
Aleksandar Prokopec <aleksandar@aleksandar-Latitude-E6500.(none)>
2012-05-31 19:24:28 +0200

    Add configs for different platforms to benchmark script.

--- benchmarks/do_benchmarks.pl
+++ benchmarks/do_benchmarks.pl
@@ -10,16 +10,64 @@ use POSIX qw(strftime);
 my $version = "v3";
 my $N = 20;
 
-my $def_lanes = [ 1.5, 2, 3, 4 ];
+my $def_lanes = [ 1, 1.5, 2, 3, 4 ];
 my $def_par   = [ 1, 2, 4, 8 ];
-my $def_b_par = [ 1, 2, 4, 8, 16, 32, 48, 64 ];
+my $def_b_par = [ 1, 2, 4, 8, 16, 32, 64 ];
 
 my $gconf = {
     wolf => {
-        SLFPInsertBench => { par => $def_par,   size => [1000000,10000000,15000000], lanes => [1] },
-        CLQInsertBench  => { par => $def_par,   size => [1000000,10000000,15000000], lanes => [1] },
-        LTQInsertBench  => { par => $def_par,   size => [1000000,10000000,15000000], lanes => [1] },
-        MLFPInsertBench => { par => $def_b_par, size => [1000000,10000000,15000000], lanes => $def_lanes }
+	SLFPInsertBench => { par => [1,2,4,8],       size => [2000000,5000000,15000000],            lanes => [1] },
+	MLFPInsertBench => { par => $def_b_par,      size => [2000000,5000000,15000000],            lanes => [1] },
+	LTQInsertBench  => { par => [1,2,4,8],       size => [2000000,5000000,15000000],            lanes => [1] },
+	CLQInsertBench  => { par => [1,2,4,8],       size => [2000000,5000000,15000000],            lanes => [1] },
+	SLFPReduceBench => { par => [1,2,4,8],       size => [2000000,5000000,10000000],            lanes => [1] },
+	MLFPReduceBench => { par => $def_b_par,      size => [2000000,5000000,10000000],            lanes => [1] },
+	LTQReduceBench  => { par => [1,2,4,8],       size => [2000000,5000000,10000000],            lanes => [1] },
+	SLFPMapBench    => { par => [1,2,4,8],       size => [2000000,5000000,10000000],            lanes => [1] },
+	MLFPMapBench    => { par => $def_b_par,      size => [2000000,5000000,10000000],            lanes => [1] },
+	LTQMapBench     => { par => [1,2,4,8],       size => [2000000,5000000,10000000],            lanes => [1] },
+	SLFPHistBench   => { par => [1,2,4,8,16],    size => [500000,1000000],            lanes => [1] },
+	MLFPHistBench   => { par => [1,2,4,8,16,32], size => [500000,1000000],            lanes => [1] },
+	LTQHistBench    => { par => [1,2,4,8,16],    size => [500000,1000000],            lanes => [1] },
+	SLFPCommBench   => { par => [1],             size => [50000000],                  lanes => [1] },
+	MLFPCommBench   => { par => $def_b_par,      size => [50000000],                  lanes => [1] },
+	LTQCommBench    => { par => [1,2,4],         size => [50000000],                  lanes => [1] },
+    },
+    maglite => {
+	SLFPInsertBench => { par => [1,2,4,8,16],    size => [1000000,2000000,5000000,15000000],            lanes => $def_lanes },
+	MLFPInsertBench => { par => $def_b_par,      size => [1000000,2000000,5000000,15000000],            lanes => $def_lanes },
+	LTQInsertBench  => { par => [1,2,4,8,16,32], size => [1000000,2000000,5000000,15000000],            lanes => $def_lanes },
+	CLQInsertBench  => { par => [1,2,4,8,16,32], size => [1000000,2000000,5000000,15000000],            lanes => $def_lanes },
+	SLFPReduceBench => { par => $def_b_par,      size => [1000000,2000000,5000000,10000000],            lanes => $def_lanes },
+	MLFPReduceBench => { par => $def_b_par,      size => [1000000,2000000,5000000,10000000],            lanes => $def_lanes },
+	LTQReduceBench  => { par => $def_b_par,      size => [1000000,2000000,5000000,10000000],            lanes => $def_lanes },
+	SLFPMapBench    => { par => $def_b_par,      size => [1000000,2000000,5000000],            lanes => $def_lanes },
+	MLFPMapBench    => { par => $def_b_par,      size => [1000000,2000000,5000000],            lanes => $def_lanes },
+	LTQMapBench     => { par => $def_b_par,      size => [1000000,2000000,5000000],            lanes => $def_lanes },
+	SLFPHistBench   => { par => [1,2,4,8,16,32], size => [500000,1000000],            lanes => $def_lanes },
+	MLFPHistBench   => { par => [1,2,4,8,16,32], size => [500000,1000000],            lanes => $def_lanes },
+	LTQHistBench    => { par => [1,2,4,8,16,32], size => [500000,1000000],            lanes => $def_lanes },
+	SLFPCommBench   => { par => [1],             size => [100000000],                  lanes => $def_lanes },
+	MLFPCommBench   => { par => $def_b_par,      size => [100000000],                  lanes => $def_lanes },
+	LTQCommBench    => { par => [1,2,4],         size => [100000000],                  lanes => $def_lanes },
+    },
+    lampmac14 => {
+	SLFPInsertBench => { par => $def_par,        size => [2000000,5000000,15000000],            lanes => $def_lanes },
+	MLFPInsertBench => { par => $def_par,        size => [2000000,5000000,15000000],            lanes => $def_lanes },
+	LTQInsertBench  => { par => $def_par,        size => [2000000,5000000,15000000],            lanes => $def_lanes },
+	CLQInsertBench  => { par => $def_par,        size => [2000000,5000000,15000000],            lanes => $def_lanes },
+	SLFPReduceBench => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	MLFPReduceBench => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	LTQReduceBench  => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	SLFPMapBench    => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	MLFPMapBench    => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	LTQMapBench     => { par => $def_par,        size => [2000000,5000000,10000000],            lanes => $def_lanes },
+	SLFPHistBench   => { par => $def_par,        size => [1500000,4000000],            lanes => $def_lanes },
+	MLFPHistBench   => { par => $def_par,        size => [1500000,4000000],            lanes => $def_lanes },
+	LTQHistBench    => { par => $def_par,        size => [1500000,4000000],            lanes => $def_lanes },
+	SLFPCommBench   => { par => $def_par,        size => [100000000],                  lanes => $def_lanes },
+	MLFPCommBench   => { par => $def_par,        size => [100000000],                  lanes => $def_lanes },
+	LTQCommBench    => { par => $def_par,        size => [100000000],                  lanes => $def_lanes },
     },
     eos =>  {
         SLFPInsertBench => { par => $def_par,   size => [1,2,3], lanes => [1] },
