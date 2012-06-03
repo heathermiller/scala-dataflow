9407f0276dc3ed00a3ff7bc23f48d9136a3d7d75
Tobias Schlatter <tobias@meisch.ch>
2012-06-03 21:06:44 +0200

    Improve on graphs, add graphs to repo

--- benchmarks/panel_fcts.R
+++ benchmarks/panel_fcts.R
@@ -1,3 +1,18 @@
+### Setup lattice settings
+font.settings <- list(fontfamily = "serif")
+
+my.theme <- list(
+                 par.xlab.text = font.settings,
+                 par.ylab.text = font.settings,
+                 par.sub.text  = font.settings,
+                 par.main.text = font.settings,
+                 par.add.text  = font.settings,
+                 par.axis.text = font.settings,
+                 add.text      = font.settings,
+                 strip.background = list(col = c('white')),
+                 strip.text    = font.settings
+                 )
+
 panel.ci <- function(x, y, ...)
 {
   x <- as.numeric(x)
@@ -18,7 +33,27 @@ panel.ci <- function(x, y, ...)
 }
 
 
-update.myopts <- function(key.labels, left = TRUE) {
+update.myopts <- function(key.labels,h=0,w=0,matrix=TRUE) {
+
+  # Setup graphical parameters
+  pars <- my.theme
+  if (matrix) {
+    pars$layout.heights <- list(strip = rep(c(0, 1), c(h-1, 1)))
+    pars$layout.widths  <- list(strip.left = rep(c(1, 0), c(1, w-1)))
+  }
+
+  # Setup strip fcts
+  top.pf <- function(which.given, ...) {
+    if (which.given == 1 & panel.number() > (h-1)*w)
+      strip.default(which.given = 1, ...)
+  }
+
+  left.pf <- function(which.given, which.panel, ...) {
+    if (which.given == 2 & panel.number() %% w == 1)
+      strip.default(which.given = 1, which.panel[2], ...)
+  }
+
+  # Update chart
   update(trellis.last.object(),
          scales = list(
            relation = "free"
@@ -26,14 +61,17 @@ update.myopts <- function(key.labels, left = TRUE) {
          pch = c(1,2,4,5),
          col = "black",
          type = "o",
-         key = list(
-           text = list(lab = key.labels),
-           points = list(pch = c(1,2,4,5)[1:length(key.labels)]),
-           fontfamily = "serif"
-           ),
+         ## key = list(
+         ##   text = list(lab = key.labels),
+         ##   points = list(pch = c(1,2,4,5)[1:length(key.labels)]),
+         ##   fontfamily = "serif"
+         ##   ),
          ylab = "Execution Time [ms]",
-         par.settings = my.theme,
-         strip = !left,
-         strip.left = left
+         par.settings = pars,
+         strip = if (matrix) top.pf else TRUE,
+         strip.left = if (matrix) left.pf else FALSE
          )
 }
+
+
+
--- benchmarks/plots.R
+++ benchmarks/plots.R
@@ -5,29 +5,6 @@ source('panel_fcts.R')
 
 library(lattice)
 
-### Setup lattice settings
-font.settings <- list(fontfamily = "serif")
-
-my.theme <- list(
-                 par.xlab.text = font.settings,
-                 par.ylab.text = font.settings,
-                 par.sub.text  = font.settings,
-                 par.main.text = font.settings,
-                 par.add.text  = font.settings,
-                 par.axis.text = font.settings,
-                 add.text      = font.settings,
-                 strip.background = list(col = c('white')),
-                 strip.text    = font.settings)
-
-## top.pf <- function(which.given, ...) {
-##   if (which.given == 1 & panel.number() >= 13)
-##     strip.default(which.given = 1, ...)
-## }
-
-## left.pf <- function(which.given, ...) {
-##   if (which.given == 2 & panel.number() %% 3 == 2)
-##     strip.default(which.given = 2, ...)
-## }
 
 ### Display CPU-scaling of Qs
 # size == max & lanef == 1
@@ -40,8 +17,9 @@ xyplot(time ~ factor(par) | arch * btype,
        scales = list(y = list(log = 10)),
        xlab = "Number of CPUs"
        )
+
 pdf("graphs/cpu-scaling.pdf", height = 10)
-update.myopts(key.labels = levels(mdat$imptype))
+update.myopts(key.labels = levels(mdat$imptype),5,3)
 dev.off()
 
 
@@ -50,21 +28,23 @@ dev.off()
 xyplot(time ~ size | arch * btype,
        data = mdat,
        groups = imptype,
-       subset = par == 1 & lanef == 1 & btype != "Comm",
+       subset = par == 1 & lanef == 1 & btype != "Comm" & btype != "Histogram",
        xlab = "Size of Benchmark")
-pdf("graphs/size-scaling-par1.pdf", height = 10)
-update.myopts(key.labels = levels(mdat$imptype))
+
+pdf("graphs/size-scaling-par1.pdf", height = 5)
+update.myopts(key.labels = levels(mdat$imptype),4,3)
 dev.off()
 
 # par == 8 & lanef == 1
 xyplot(time ~ size | arch * btype,
        data = mdat,
        groups = imptype,
-       subset = par == 8 & lanef == 1 & btype != "Comm",
+       subset = par == 8 & lanef == 1 & btype != "Comm" & btype != "Histogram",
        xlab = "Size of Benchmark",
 )
-pdf("graphs/size-scaling-par8.pdf", height = 10)
-update.myopts(key.labels = levels(mdat$imptype))
+
+pdf("graphs/size-scaling-par8.pdf", height = 5)
+update.myopts(key.labels = levels(mdat$imptype),4,3)
 dev.off()
 
 ### Display lane-factor-scaling of Qs
@@ -76,6 +56,7 @@ xyplot(time ~ factor(lanef) | btype,
                 machine != "wolf" & imptype == "Multi-Lane FlowPool",
        xlab = "Factor of Lanes"
        )
-pdf("graphs/lanef-scaling.pdf", height = 10)
-update.myopts(key.labels = levels(mdat$arch)[1:2], left = FALSE)
+
+pdf("graphs/lanef-scaling.pdf", height = 5)
+update.myopts(key.labels = levels(mdat$arch)[1:2], matrix = FALSE)
 dev.off()
