56002fe36d85295c69502918d304c23d01a2dc07
Tobias Schlatter <tobias@meisch.ch>
2012-12-01 12:52:08 +0100

    Add more tests

--- src/test/scala/scala/dataflow/array/FlowArraySliceViewSuite.scala
+++ src/test/scala/scala/dataflow/array/FlowArraySliceViewSuite.scala
@@ -10,6 +10,7 @@ class FlowArraySliceViewSuite extends FunSuite with FATestHelper {
 
   val slStart = size / 9
   val slEnd   = 8 * size / 9
+  val slSize  = slEnd - slStart + 1
 
   def nSL = nFA.slice(slStart, slEnd)
 
@@ -22,6 +23,20 @@ class FlowArraySliceViewSuite extends FunSuite with FATestHelper {
     val fa1 = nSL
     val fa2 = fa1.map(_ * 2)
     verEls(fa2)((x,i) => x == (i + slStart) * 2)
+  }
+
+  test("fold on slice") {
+    val fa1 = nSL
+    val res = fa1.fold(0)(_ + _)
+    val v = res.blocking
+    assert(v == (slEnd + 1) * slEnd / 2 - (slStart - 1) * slStart / 2)
+  }
+
+  test("zipMap on FASliceView and FlatFA") {
+    val sl = nSL
+    val fl = nFA(slSize)
+    val res = (sl zipMap fl) { (x,y) => x + y }
+    verEls(res)((x,i) => x == 2 * i + slStart)
   }
 
 }
