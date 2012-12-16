b7fa626cee104611099e4a5b3b6277a8a67704be
Tobias Schlatter <tobias@meisch.ch>
2012-12-16 23:59:29 +0100

    Add unit test for matrix matrix product

--- src/test/scala/scala/dataflow/la/LASuite.scala
+++ src/test/scala/scala/dataflow/la/LASuite.scala
@@ -10,6 +10,7 @@ import scala.dataflow.array.FATestHelper
 @RunWith(classOf[JUnitRunner])
 class LASuite extends FunSuite with FATestHelper with ArrayLA with FlowArrayImpl {
 
+  override val timeout = 10000L // 10s
   val n = 200
 
   test("create a matrix") {
@@ -33,6 +34,13 @@ class LASuite extends FunSuite with FATestHelper with ArrayLA with FlowArrayImpl
     val v = ones(n)
     val res = m * v
     verEls(res.data)((x,i) => x == n)
+  }
+
+  test("matrix matrix product") {
+    val M = ones(n, 2*n)
+    val N = ones(2*n, 3*n)
+    val res = M * N
+    verEls(res.data)((x,i) => x == 2 * n)
   }
 
 }
