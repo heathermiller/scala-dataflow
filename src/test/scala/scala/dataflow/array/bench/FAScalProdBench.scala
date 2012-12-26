6ab7a424c913a14539ae6c328ba6abd75129f3c5
Tobias Schlatter <tobias@meisch.ch>
2012-12-26 09:03:45 +0100

    First scala-meter benchmark

--- src/test/scala/scala/dataflow/array/bench/FAScalProdBench.scala
+++ src/test/scala/scala/dataflow/array/bench/FAScalProdBench.scala
@@ -2,10 +2,28 @@ package scala.dataflow.array.bench
 
 import scala.dataflow.Utils
 import scala.dataflow.array._
+import org.scalameter.api._
 
-object FAScalProdBench extends testing.Benchmark with Utils.Props {
+object FAScalProdBench extends PerformanceTest {
 
-  def run {
+  lazy val executor = SeparateJvmsExecutor(
+    org.scalameter.Executor.Warmer.Default(),
+    Aggregator.min,
+    new Measurer.Default
+  )
+
+  lazy val reporter  = new LoggingReporter
+  lazy val persistor = Persistor.None
+
+  val sizes = Gen.range("size")(1000, 10000, 2000)
+
+  performance of "FlowArray" in {
+    measure method "Scalar Product" in {
+      using(sizes) in { sz => scp(sz) }
+    }
+  }
+
+  private def scp(size: Int) {
     val x = FlowArray.tabulate(size)(x => x*x)
     val y = FlowArray.tabulate(size)(x => x*x)
 
@@ -13,7 +31,7 @@ object FAScalProdBench extends testing.Benchmark with Utils.Props {
     
     val res = p.fold(0)(_ + _)
 
-    println(res.blocking)
+    res.blocking
   }
 
 }
