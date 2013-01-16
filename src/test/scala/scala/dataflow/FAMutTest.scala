ffe8c5bb064335b8c2cf3e7568a5a0ecf1886129
Tobias Schlatter <tobias@meisch.ch>
2013-01-16 17:01:54 +0100

    Fix bug in FAMutTest (due to change of interface)

--- src/test/scala/scala/dataflow/FAMutTest.scala
+++ src/test/scala/scala/dataflow/FAMutTest.scala
@@ -7,8 +7,8 @@ object FAMutTest extends App {
   val maxVal = 1000000
   val size = math.ceil(math.log(maxVal) / math.log(2)).toInt + 1
 
-  val fa1 = FlowArray.tabulate(maxVal)(x => new HIter(x))
-  val fa2 = fa1.mutConverge(_.v <= 1)(x => x.adv(x.v / 2))
+  val fa1 = FlowArray.tabulate(maxVal)(x => x)
+  val fa2 = fa1.mutConverge(x => new HIter(x))(x => x)(_.v <= 1)(x => x.adv(x.v / 2))
   val fa3 = fa2.map(_.dat)
   val f   = fa3.fold(Array.fill(size)(0.0)) { (x1, x2) =>
     (x1 zip x2) map {
